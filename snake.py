79de26d602d0911c544e23781afa7c635cd1eb32
eytangut <eytangut@gmail.com>
2021-01-12 14:19:37 +0200

    snake

--- snake.py
+++ snake.py
@@ -15,10 +15,45 @@ GREEN = (0, 255, 0)
 DARKGREEN = (0, 155, 0)
 DARKGREY = (40, 40, 40)
 BGCOLOR = BLACK
-up = 'up'
-down = 'down'
-left = 'left'
-right = 'right'
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
 HEAD = 0
 def main():
-    
+    global FPSCLOCK, DISPLAYSURF, BASICFONT
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    BASICFONT = pygame.font.Font('assets/freesansbold.ttf', 18)
+    pygame.display.set_caption("Wormy")
+    showStartScreen()
+    while True:
+        runGame()
+        showGameOverScreen()
+def runGame():
+    startx = random.randint(5, CELLWIDTH - 6)
+    starty = random.randint(5, CELLHEIGHT - 6)
+    wormCoords = [{'x': startx,     'y': starty},
+                  {'x': startx - 1, 'y': starty},
+                  {'x': startx - 2, 'y': starty}]
+    direction = RIGHT
+    apple = getRandomLocation()
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            elif event.type == KEYDOWN:
+                if (event.key == K_LEFT or event.key == K_a) and direction != RIGHT:
+                    direction = LEFT
+                elif (event.key == K_RIGHT or event.key == K_d) and direction != LEFT:
+                    direction = RIGHT
+                elif (event.key == K_UP or event.key == K_w) and direction != DOWN:
+                    direction = UP
+                elif (event.key == K_DOWN or event.key == K_s) and direction != UP:
+                    direction = DOWN
+                elif event.key == K_ESCAPE:
+                    terminate()
+        if wormCoords[HEAD]['x'] == -1 or wormCoords[HEAD]['x'] == CELLWIDTH or wormCoords[HEAD]['y'] == -1 or wormCoords[HEAD]['y'] == CELLHEIGHT:
+            return
+        for wormBody in wormCoords[]
